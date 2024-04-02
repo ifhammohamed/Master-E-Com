804b980b452dae0d662a7762e58bd66296d267ae
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 20:40:22 +0530

    Add password encryption and decryption methods to user model

--- models/user.model.js
+++ models/user.model.js
@@ -27,6 +27,7 @@ var userSchema = new mongoose.Schema({
   },
 });
 
+// Encrypt the password
 userSchema.pre("save", async function (next) {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -37,6 +38,15 @@ userSchema.pre("save", async function (next) {
     next(error);
   }
 });
+
+// Decrypt and check the password
+userSchema.methods.isPasswordMatched = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 //Export the model
 module.exports = mongoose.model("user", userSchema);
