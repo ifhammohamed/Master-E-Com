d0d2121d6ab0fe077f6229b025c2f7e55f35dcfc
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 11:32:09 +0530

    Add register route to auth.routes.js

---
+++ routes/auth.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/register");
+//     , async (req, res) => {
+//   try {
+//     const { first_name, last_name, email, mobile, password } = req.body;
+//     if (!first_name || !last_name || !email || !mobile || !password) {
+//       return res.status(400).json({ error: "All fields are required" });
+//     }
+//     const userExist = await User.findOne({ email });
+//     if (userExist) {
+//       return res.status(400).json({ error: "User already exists" });
+//     }
+//     const user = new User({ first_name, last_name, email, mobile, password });
+//     await user.save();
+//     res.status(201).json({ message: "User registered successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// }
+
+module.exports = router;
