0a7e1a0818542c7e8a537ef3e7451ae20ad348b1
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 12:18:35 +0530

    Add bodyParser middleware to handle JSON and URL-encoded data

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express = require("express");
 require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
 const authRouter = require("./routes/auth.routes");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 2000;
 
 const app = express();
 dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/apo/user", authRouter);
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
