186d56ae22bb3c520b622d93505926ee430c411d
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 11:32:17 +0530

    Add authRouter to handle user authentication

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express");
 // const dotenv = require('dotenv').config();
 require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
+const authRouter = require("./routes/auth.routes");
 
 const PORT = process.env.PORT || 2000;
 
 const app = express();
 dbConnect();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/apo/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
