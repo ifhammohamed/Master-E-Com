900f131de9590f89fc319a29b0a0bd99f7d04cf5
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 11:11:31 +0530

    Add route for root endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 2000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
