25f07988d67498ca5a783e5febb9f462059c1c41
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 19:07:58 +0530

    Add error handling middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
 const authRouter = require("./routes/auth.routes");
 const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 const PORT = process.env.PORT || 2000;
 
@@ -13,6 +14,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
