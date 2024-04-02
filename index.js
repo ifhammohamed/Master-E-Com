eea04c4541d8c3d45747ab78d1b3f5391d5277ea
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 21:51:43 +0530

    Remove commented out code and update dotenv configuration

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const dotenv = require('dotenv').config();
 require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
 const authRouter = require("./routes/auth.routes");
