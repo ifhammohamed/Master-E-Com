215b114f3e7eb52f9d4f732def6080eb9819e1a9
Mohamed MRI <mohamedmri.21@uom.lk>
2024-04-02 11:20:38 +0530

    Add database connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 // const dotenv = require('dotenv').config();
 require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
 
 const PORT = process.env.PORT || 2000;
 
 const app = express();
+dbConnect();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
